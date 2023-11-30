dde56cea37eee52194564f1abdbc2c1effc4a957
Sandip Mishra <sandipm02@icloud.com>
2023-11-30 00:10:27 -0700

    Added Hamburger

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Container, Grid, GridCol, Image, TextInput } from '@mantine/core';
 import BottomNavBar from './bottom-nav.js';
 import MiniFish from '../assets/minifish.png'
+import { GiHamburgerMenu } from "react-icons/gi";
 import '../styling/login.css';
 import '../styling/dashboard.css';
 
@@ -16,6 +17,7 @@ const Dashboard = () => {
           <GridCol className="contain-content" style={{ marginBottom: '15px', display: 'flex', alignItems: 'center' }}>
             <Image src={MiniFish} alt="Minifish Logo" width={40} height={25} />
             <strong><h1 className="login-text-2" style={{ marginLeft: '5px' }}>Discovery Map</h1></strong>
+            <GiHamburgerMenu style={{ marginLeft: 'auto', cursor: 'pointer', fontSize: '24px' }} />
           </GridCol>
           <GridCol span={12}>
             <TextInput
