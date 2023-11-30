ccd50f98163a84b931071439709d1680db56750a
Sandip Mishra <sandipm02@icloud.com>
2023-11-30 00:23:56 -0700

    Added some changes to dashboard

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -1,31 +1,51 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Container, Grid, GridCol, Image, TextInput } from '@mantine/core';
 import BottomNavBar from './bottom-nav.js';
-import MiniFish from '../assets/minifish.png'
-import { GiHamburgerMenu } from "react-icons/gi";
+import MiniFish from '../assets/minifish.png';
+import { GiHamburgerMenu } from 'react-icons/gi';
+import { useAuth } from './authcontext.js'; // Adjust the path to the AuthContext file
+import { useNavigate } from 'react-router-dom'; // Assuming you are using React Router v6
 import '../styling/login.css';
 import '../styling/dashboard.css';
 
 const Dashboard = () => {
+  const { user, login } = useAuth();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (user === null) {
+      navigate('/login');
+    }
+    else {
+      console.log(user);
+    }
+  }, [user, navigate]);
+
   return (
     <div className='contain-dash'>
-      <Container size="xs">
+      <Container size='xs'>
         <Grid className='cont'>
           <br />
           <br />
           <br />
-          <GridCol className="contain-content" style={{ marginBottom: '15px', display: 'flex', alignItems: 'center' }}>
-            <Image src={MiniFish} alt="Minifish Logo" width={40} height={25} />
-            <strong><h1 className="login-text-2" style={{ marginLeft: '5px' }}>Discovery Map</h1></strong>
-            <GiHamburgerMenu style={{ marginLeft: 'auto', cursor: 'pointer', fontSize: '24px' }} />
+          <GridCol className='contain-content' style={{ marginBottom: '15px', display: 'flex', alignItems: 'center' }}>
+            <Image src={MiniFish} alt='Minifish Logo' width={50} height={30} />
+            <strong>
+              <h1 className='login-text-2' style={{ marginLeft: '5px' }}>
+                Discovery Map
+              </h1>
+            </strong>
+            <GiHamburgerMenu
+              style={{ marginLeft: 'auto', cursor: 'pointer', fontSize: '24px' }}
+            />
           </GridCol>
           <GridCol span={12}>
             <TextInput
               className='input-dash'
-              placeholder="🔎 Search"
-              autoComplete="username"
+              placeholder='🔎 Search'
+              autoComplete='username'
               required
-              name="search"
+              name='search'
               styles={{
                 input: {
                   appearance: 'none',
@@ -33,7 +53,7 @@ const Dashboard = () => {
                   border: 'none',
                   borderBottom: '1px solid transparent',
                   boxShadow: 'none',
-                  WebkitBoxShadow: 'none'
+                  WebkitBoxShadow: 'none',
                 },
               }}
             />
